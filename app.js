eb476682723a42c1e0471bc0fd9ad6709946d60f
Sávio Soares <dsaviosnogueira@gmail.com>
2024-02-17 21:23:45 -0300

    Finished project

--- app.js
+++ app.js
@@ -20,7 +20,60 @@ function generatorHands() {
     return;
   }
 
+  // Limpar a lista antes de adicionar novos itens
+  clearList();
+
   for (let letter of inpForm) {
-    console.log(letter);
+    const lowercaseLetter = letter.toLowerCase();
+    switch (lowercaseLetter) {
+      case "a":
+      case "b":
+      case "c":
+      case "d":
+      case "e":
+      case "f":
+      case "g":
+      case "h":
+      case "i":
+      case "j":
+      case "k":
+      case "l":
+      case "m":
+      case "n":
+      case "o":
+      case "p":
+      case "q":
+      case "r":
+      case "s":
+      case "t":
+      case "u":
+      case "v":
+      case "w":
+      case "x":
+      case "y":
+      case "z":
+        createList(letter);
+        break;
+      // Adicione outros casos conforme necessário
+    }
+  }
+
+  function createList(letter) {
+    const newLi = document.createElement("li");
+    const newImg = document.createElement("img");
+
+    newImg.src = `./img/${letter.toUpperCase()}.png`;
+    newLi.append(newImg);
+
+    document.getElementById("container-list").appendChild(newLi);
+  }
+
+  // Função para limpar a lista
+  function clearList() {
+    const containerList = document.getElementById("container-list");
+    // Remover todos os elementos filhos do container-list
+    while (containerList.firstChild) {
+      containerList.removeChild(containerList.firstChild);
+    }
   }
 }
